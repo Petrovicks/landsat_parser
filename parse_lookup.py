e150cc98aca5cb7b22ad6fa78fc2378a7a44558a
Petrovicks <41557867+Petrovicks@users.noreply.github.com>
2019-01-24 17:43:17 -0500

    Changed the logic to give you a temperature based on A,B and thermistor values. This method attempts to interpolate using averages. A more robust alternative could possibly be multiple linear regression.

--- parse_lookup.py
+++ parse_lookup.py
@@ -6,34 +6,36 @@ def return_fit_line(x,y):
 	m,b = np.polyfit(x,y,1) #Linear fit
 	return [m,b]
 
-lookup_voltage = []
 lookup_A = []
 lookup_B = []
+lookup_thermistor = []
 lookup_unit = []
 
 fit_A = np.array(0)
 fit_B = np.array(0)
-fit_unit = np.array(0)
+fit_thermistor = np.array(0)
 
-rawLookupDataDict = [row for row in csv.DictReader(open('example_lookup.csv'), ('Voltage', 'A', 'B', 'Unit'))]
+rawLookupDataDict = [row for row in csv.DictReader(open('example_lookup.csv'), ('A', 'B', 'Thermistor', 'Unit'))]
 rawLookupData = [row for row in csv.reader(open('example_lookup.csv'))]
 #print("num of observations: ", len(rawLookupData)-1)
 
-keyedLookupData = {}
+keyedLookupA = {}
+keyedLookupB = {}
+keyedLookupThermistor = {}
 for row in rawLookupData[1:]:
-	lookup_voltage.append(int(row[0]))
-	lookup_A.append(float(row[1]))
-	lookup_B.append(float(row[2]))
+	lookup_A.append(float(row[0]))
+	lookup_B.append(float(row[1]))
+	lookup_thermistor.append(float(row[2]))
 	lookup_unit.append(float(row[3]))
 for row in rawLookupDataDict[1:]:
-	keyedLookupData[row['Voltage']] = row
+	keyedLookupA[row['A']] = row
+	keyedLookupB[row['B']] = row
+	keyedLookupThermistor[row['Thermistor']] = row
 #print("Values for 6V: ", keyedLookupData['6'])
 
-print(lookup_voltage)
-
-fit_A = return_fit_line(lookup_voltage, lookup_A)
-fit_B = return_fit_line(lookup_voltage, lookup_B)
-fit_unit = return_fit_line(lookup_voltage, lookup_unit)
+fit_A = return_fit_line(lookup_A, lookup_unit)
+fit_B = return_fit_line(lookup_B, lookup_unit)
+fit_thermistor = return_fit_line(lookup_thermistor, lookup_unit)
 
 #print("Interpolated value at 2V for B: ", round(fit_B[0]*2 + fit_B[1]))
 
@@ -44,18 +46,29 @@ with open('example_output.csv', mode = 'w') as output_file:
 
 		all = []
 		row = next(field_values_reader)
-		row.extend(['A','B', 'Unit'])
+		row.extend(['Unit'])
 		all.append(row)
 		for row in field_values_reader:
-			if row[1] in keyedLookupData:
-				value = keyedLookupData[row[1]]
-				row.append(value['A'])
-				row.append(value['B'])
-				row.append(value['Unit'])
+			if row[1] in keyedLookupA:
+				value = keyedLookupA[row[1]]
+				temp_at_A = value['Unit']
 			else:
-				row.append(round(float(row[1])*float(fit_A[0])+fit_A[1],1))
-				row.append(round(float(row[1])*float(fit_B[0])+fit_B[1],1))
-				row.append(round(float(row[1])*float(fit_unit[0])+fit_unit[1],1))
+				temp_at_A = round(float(row[1])*float(fit_A[0])+fit_A[1],4)
+
+			if row[2] in keyedLookupB:
+				value = keyedLookupB[row[2]]
+				temp_at_B = value['Unit']
+			else:
+				temp_at_B = round(float(row[2])*float(fit_B[0])+fit_B[1],4)
+
+			if row[3] in keyedLookupThermistor:
+				value = keyedLookupThermistor[row[3]]
+				temp_at_therm = value['Unit']
+			else:
+				temp_at_therm = round(float(row[3])*float(fit_thermistor[0])+fit_thermistor[1],4)
+
+			all_temps = [temp_at_B, temp_at_B, temp_at_therm]
+			row.append(round(np.average(all_temps)))
 			all.append(row)
 
 		output_writer.writerows(all)
